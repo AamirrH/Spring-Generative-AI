d9f52e7ca814b8b56630ffc207238235695828a6
AamirrH <aamirr.1704@gmail.com>
2026-06-22 20:41:21 +0530

    init : initialise repository

---
+++ Spring-AI-Tutorial/src/test/java/com/code/prodapp/springaitutorial/SpringAiTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.prodapp.springaitutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
